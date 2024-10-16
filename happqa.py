6fffe150be5d36bcd2d225caff1186877ffc5bf3
Vindya-Diddeniya <dvhansi@gmail.com>
2024-10-16 10:53:05 +0300

    Update happqa.py

--- happqa.py
+++ happqa.py
@@ -1,3 +1,14 @@
+#!/usr/bin/env python
+
+"""Happy QA
+This script solves how many releases possible within a 2-week sprint (10 working days) with the following constraints:
+       1.Any taken release must be completed within the sprint.
+       2.Bob can only validate one release at a time.
+       3.Release validation cannot be postponed.
+
+I'll use a greedy algorithm to solve this problem.
+python: 3.13
+"""
 def select_releases(releases_file):
 # Read input file
     with open(releases_file, 'r') as f:
@@ -26,3 +37,38 @@ def select_releases(releases_file):
 
 # Run the algorithm
 select_releases('releases.txt')
+
+
+"""
+Postponing Release Validation (Optional)
+"""
+
+def select_releases_postponed(releases_file):
+# Read input file
+    with open(releases_file, 'r') as f:
+        releases = [tuple(map(int, line.split())) for line in f.readlines()]
+
+    #sorted releases
+    for delivery_day, validation_time in releases:
+        if delivery_day + validation_time > 10:
+            #Try to postpone validation
+            for day in range(delivery_day, 11):
+                if day + validation_time <= 10:
+                    # Add release to selected list with postponed validation
+                    selected_releases.append((day, day + validation_time - 1))
+                # Update current day
+                current_day = day + validation_time
+                break
+        else:
+            #Add release to selected list without postponing
+            selected_releases.append((delivery_day, delivery_day + validation_time - 1))
+            # Update current day
+            current_day = delivery_day + validation_time
+        # Write output to file
+        with open('solution.txt', 'w') as f:
+            f.write(str(len(selected_releases)) + '\n')
+        for release in selected_releases:
+            f.write(' '.join(map(str, release)) + '\n')
+
+# Run the algorithm
+select_releases_postponed('releases.txt')
